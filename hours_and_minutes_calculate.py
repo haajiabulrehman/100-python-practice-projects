ec3384ad5e70ad980633dc44f553b559939052e2
haaji abul rehman <126969489+haajiabulrehman@users.noreply.github.com>
2025-09-14 12:45:21 +0500

    1st project of time cacuator

---
+++ hours_and_minutes_calculate.py
@@ -0,0 +1,19 @@
+
+      # 1. calculate how many minutes are in 20 hours
+
+
+one_hour = 60
+hours = 20
+minutes = hours*one_hour
+print(minutes)
+print(type(minutes))
+
+
+
+       # 2. calculate how many hours in 460 minutes
+print("this is second problem")
+one_hours = 60
+minute = 460
+hours_in_given_minutes = 460/60
+print(hours_in_given_minutes)
+print(type(hours_in_given_minutes))
